a60de3b1c579bfa07cd055f6e4bcd3b04de92a3e
bShinzato <bshinzato@outlook.com>
2023-07-19 11:22:50 -0700

    added all components/pages files with minimum code to render

---
+++ src/pages/RecipeIndex.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RecipeIndex = () => {
+  return (
+    <div>RecipeIndex</div>
+  )
+}
+
+export default RecipeIndex
---
+++ src/pages/RecipeShow.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RecipeShow = () => {
+  return (
+    <div>RecipeShow</div>
+  )
+}
+
+export default RecipeShow

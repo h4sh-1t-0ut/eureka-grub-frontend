53012943aa6e6c7be3eb954a275e1892169e8694
bShinzato <bshinzato@outlook.com>
2023-07-19 10:46:31 -0700

    added react-router-dom, reactstrap, mockdata for users and recipes, and added folders into src.

---
+++ src/mockRecipes.js
@@ -0,0 +1,45 @@
+let mockRecipes = [
+    {
+        id: 1,
+        name: "Lumpia",
+        instructions: "Heat up a pan with 2 inches of oil over medium high heat. Shred the onions and carrots and set aside. Wisk eggs (glue) in a bowl. Take your ground pork (filling) and put it into a large bowl. Put in the onions, carrots, and seasoning into the bowl and mix with hand. Take your lumpia wrapper and cut it with scissors from corner to corner (triangles). Afterwards put two spoon full of filling and place it on the wrapper towards the bottom of the triangle in the middle. Form a rectangular shape with the filling. Wrap from the bottom until filling is covered. Use your glue and smear it onto the top side of the triangle that is not rolled yet. Take the sides of the wrapper and fold inwards and continue to roll. Throw it into the pan and wait until it is golden brown! Remove excess oil, and place on a paper towel covered plate. Eat it with sweet chilli sauce!",
+        cook_time: "30 Minutes",
+        ingredients:"1lb ground pork, 1/2 white onion, 2 carrots, 1/3 tsp black pepper and garlic seasoning, 1/2 tsp salt, lumpia wrapper, 2 eggs, coconut oil, sweet chilli sauce",
+        course: "Appetizer",
+        servings: 1,
+        allergies: "*",
+        tutorial_link:"",
+        recipe_image:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fomnivorescookbook.com%2Flumpia-shanghai%2F&psig=AOvVaw1avLjLoqIMNTu59hcV1NJV&ust=1689814111816000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCKikqqLGmYADFQAAAAAdAAAAABAE",
+        user_id: 1
+      },
+
+    {
+        id: 1,
+        name: "Lumpia",
+        instructions: "Heat up a pan with 2 inches of oil over medium high heat. Shred the onions and carrots and set aside. Wisk eggs (glue) in a bowl. Take your ground pork (filling) and put it into a large bowl. Put in the onions, carrots, and seasoning into the bowl and mix with hand. Take your lumpia wrapper and cut it with scissors from corner to corner (triangles). Afterwards put two spoon full of filling and place it on the wrapper towards the bottom of the triangle in the middle. Form a rectangular shape with the filling. Wrap from the bottom until filling is covered. Use your glue and smear it onto the top side of the triangle that is not rolled yet. Take the sides of the wrapper and fold inwards and continue to roll. Throw it into the pan and wait until it is golden brown! Remove excess oil, and place on a paper towel covered plate. Eat it with sweet chilli sauce!",
+        cook_time: "30 Minutes",
+        ingredients:"1lb ground pork, 1/2 white onion, 2 carrots, 1/3 tsp black pepper and garlic seasoning, 1/2 tsp salt, lumpia wrapper, 2 eggs, coconut oil, sweet chilli sauce",
+        course: "Appetizer",
+        servings: 1,
+        allergies: "*",
+        tutorial_link:"",
+        recipe_image:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fomnivorescookbook.com%2Flumpia-shanghai%2F&psig=AOvVaw1avLjLoqIMNTu59hcV1NJV&ust=1689814111816000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCKikqqLGmYADFQAAAAAdAAAAABAE",
+        user_id: 2
+    },
+
+    {
+        id: 1,
+        name: "Lumpia",
+        instructions: "Heat up a pan with 2 inches of oil over medium high heat. Shred the onions and carrots and set aside. Wisk eggs (glue) in a bowl. Take your ground pork (filling) and put it into a large bowl. Put in the onions, carrots, and seasoning into the bowl and mix with hand. Take your lumpia wrapper and cut it with scissors from corner to corner (triangles). Afterwards put two spoon full of filling and place it on the wrapper towards the bottom of the triangle in the middle. Form a rectangular shape with the filling. Wrap from the bottom until filling is covered. Use your glue and smear it onto the top side of the triangle that is not rolled yet. Take the sides of the wrapper and fold inwards and continue to roll. Throw it into the pan and wait until it is golden brown! Remove excess oil, and place on a paper towel covered plate. Eat it with sweet chilli sauce!",
+        cook_time: "30 Minutes",
+        ingredients:"1lb ground pork, 1/2 white onion, 2 carrots, 1/3 tsp black pepper and garlic seasoning, 1/2 tsp salt, lumpia wrapper, 2 eggs, coconut oil, sweet chilli sauce",
+        course: "Appetizer",
+        servings: 1,
+        allergies: "*",
+        tutorial_link:"",
+        recipe_image:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fomnivorescookbook.com%2Flumpia-shanghai%2F&psig=AOvVaw1avLjLoqIMNTu59hcV1NJV&ust=1689814111816000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCKikqqLGmYADFQAAAAAdAAAAABAE",
+        user_id: 3
+    }
+]
+
+export default mockRecipes;

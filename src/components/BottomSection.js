5b97f8108906269c2607e584f7308b5255618539
Ricky Espinoza <rickyjespinoza@gmail.com>
2023-07-26 11:08:20 -0700

    more editing

--- src/components/BottomSection.js
+++ src/components/BottomSection.js
@@ -9,24 +9,9 @@ const fadeImages = [
 ]
 
 const BottomSection = () => {
-  // var myIndex = 0;
-  // carousel();
-
-  // function carousel() {
-  //   var i;
-  //   var x = document.getElementsByClassName("mySlides");
-  //   for (i = 0; i < x.length; i++) {
-  //     x[i].style.display = "none";  
-  //   }
-  //   myIndex++;
-  //   if (myIndex > x.length) {myIndex = 1}    
-  //   x[myIndex-1].style.display = "block";  
-  //   setTimeout(carousel, 4000); // Change image every 2 seconds
-  // }
 
   return (
     <>
-    
     <div className="slide-container1">
       <Fade>
         <div className="each-fade">
